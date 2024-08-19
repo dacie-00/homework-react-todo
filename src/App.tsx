7ff2efe3b6e3321f9a2b95b10a9aeb1675d6aa87
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-08-19 21:23:19 +0300

    Load todo items from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from "react";
+
+type TodoItem = {
+    name: string,
+    comment: string,
+    due_date: string,
+    completed_at: string,
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    async function getData() {
+        const url = "http://localhost:3004/tasks";
 
-  return (
+        const response = await fetch(url);
+
+        const json = await response.json();
+        return json;
+    }
+
+    const [todoItems, setTodoItems]: TodoItem = useState(null)
+
+    useEffect(() => {
+        getData().then((items) =>
+            setTodoItems(items)
+        )
+    }, [])
+
+
+    return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {todoItems && todoItems.map((item, index) => (<div key={index}>{item.name}</div>))}
     </>
-  )
+    )
 }
 
 export default App
